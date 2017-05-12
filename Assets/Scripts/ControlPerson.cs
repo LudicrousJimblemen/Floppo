f180a7b8e0deb77bf6255a81502f8d4877135d3b
Poyo <poyoarya@gmail.com>
2017-05-12 14:03:53 -0700

    input update

--- Assets/Scripts/ControlPerson.cs
+++ Assets/Scripts/ControlPerson.cs
@@ -7,16 +7,9 @@ public class ControlPerson : MonoBehaviour {
 	public Person Person;
 	
 	private void Update() {
-		bool w = Input.GetKey(KeyCode.W);
-		bool s = Input.GetKey(KeyCode.S);
-		bool e = Input.GetKey(KeyCode.E);
-		bool q = Input.GetKey(KeyCode.Q);
-		bool d = Input.GetKey(KeyCode.D);
-		bool a = Input.GetKey(KeyCode.A);
-		
 		Person.Act(
-			w || s ? (w ? 1 : -1) : 0,
-			e || q ? (e ? 1 : -1) : 0,
-			d || a ? (d ? 1 : -1) : 0);
+			Input.GetAxis("X"),
+			Input.GetAxis("Y"),
+			Input.GetAxis("Z"));
 	}
 }
